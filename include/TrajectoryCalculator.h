53acf3e12a2c12055da3154c0630facffe89ca6e
JanTopolewski <jankotopolewski@gmail.com>
2025-12-03 20:59:42 +0100

    Add docstrings to some important methods

--- include/TrajectoryCalculator.h
+++ include/TrajectoryCalculator.h
@@ -16,7 +16,13 @@ public:
 	std::vector<double> getYAxisCoordinates() const;
 	std::string getWarning() const;
 	
-
+	/**
+	 * @brief
+	 * This function manages calculations of ball's trajectory by using different methods(like RK4)
+	 * depending on the particular case in order to make calculations as fast as possible. It saves results of calculations in
+	 * xAxisCoordinates and yAxisCoordinates vectors and an additional information about problems with calculations(like a never
+	 * ending case) to warning variable
+	 */
 	void CalculateData(
 		double ballVelocity,
 		double firingAngle,
@@ -94,7 +100,10 @@ public:
 		double ballMass
 	);
 
-
+	/**
+	 * @brief
+	 * These calculations are for calculating position and velocity changes in each step of RK4
+	 */
 	void RungeKuttaMethodPart(
 		function<void()> calculatingFunc,
 		double& kX,
