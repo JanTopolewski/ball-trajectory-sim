916a4a7d45bf8ff2387764de07995a98da6d1640
JanTopolewski <jankotopolewski@gmail.com>
2026-02-26 23:14:43 +0100

    Update Simulation.h in order to include data from 3D simulations

--- include/Simulation.h
+++ include/Simulation.h
@@ -4,19 +4,24 @@
 
 struct Simulation{
     float ballVelocity;
-    float firingAngle;
+    float horizontalAngle;
+    float verticalAngle;
     float ballRadius;
     float ballMass;
     float gravitationalAcceleration;
     float windVelocity;
-    float windAngle;
+    float windHorizontalAngle;
+    float windVerticalAngle;
     float atmosfericDensity;
     float initialDistanceFromGround;
     std::vector<double> xAxisCoordinates;
     std::vector<double> yAxisCoordinates;
+    std::vector<double> zAxisCoordinates;
     std::string warning;
     std::vector<double> xAxisAdditionalTrajectory;
     std::vector<double> yAxisAdditionalTrajectory;
+    std::vector<double> zAxisAdditionalTrajectory;
     bool hasTarget;
     float targetDistance;
+    float targetHorizontalAngle;
 };
